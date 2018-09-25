ff88dba10d667804ac60745fa5488484654149ac
tolian <toliankir@gmail.com>
2018-09-25 14:28:22 +0300

    Add up button to ps3 landing

--- ps3_jquery/landing/script.js
+++ ps3_jquery/landing/script.js
@@ -12,3 +12,21 @@ function scrollToClass(targetClass) {
 
    body.animate({scrollTop: scrollPosition}, 500);
 }
+
+    $(document).scroll(() => {
+        const scrollUpButton = $('.button-up');
+        if ($(this).scrollTop() === 0) {
+            if (scrollUpButton.css('opacity') === '1') {
+                scrollUpButton.animate({opacity: 0}, 500, () => {
+                    scrollUpButton.css('display', 'none');
+                });
+            }
+        } else {
+            scrollUpButton.css('display', 'block');
+            if (scrollUpButton.css('opacity') === '0') {
+                scrollUpButton.animate({opacity: 1}, 500, () => {
+
+                });
+            }
+        }
+    });
--- ps3_jquery/select/script.js
+++ ps3_jquery/select/script.js
@@ -69,3 +69,5 @@ function customSelectInit(selectData) {
         });
     }
 }
+
+
