0098fadc501f4ad22346b51365182eac4d575cc7
tolian <toliankir@gmail.com>
2018-09-25 11:24:10 +0300

    Add constants to ps3 select

--- ps3_jquery/select/script.js
+++ ps3_jquery/select/script.js
@@ -1,4 +1,9 @@
 let animationEnd = true;
+const animationDelay = 500;
+const CUSTOM_SELECT_OBJ = 'div.custom-select';
+const CUSTOM_SELECT_CONTENT_OBJ = 'div.custom-select__content';
+const SLIDER_OBJ = 'div.slider';
+
 const selectData = [
     {
         title: "test 1",
@@ -24,39 +29,39 @@ const selectData = [
 customSelectInit(selectData);
 
 function customSelectInit(selectData) {
-    const newSlider = document.createElement('div');
-    selectData.forEach((element) => {
+    const slider = document.createElement('div');
+    selectData.forEach((selectItemData) => {
         const newSliderItem = document.createElement('div');
-        const elementContent = `<img class="custom-select__image" src="${element.image}">${element.title}`;
+        const elementContent = `<img class="custom-select__image" src="${selectItemData.image}">${selectItemData.title}`;
         $(newSliderItem).addClass('slider__item').html(elementContent).click(() => {
-                $('div.custom-select__content').
+                $(CUSTOM_SELECT_CONTENT_OBJ).
                 addClass('custom-select__content--selected').
                 html(elementContent);
             }
-        ).appendTo($(newSlider));
+        ).appendTo($(slider));
     });
 
-    $(newSlider).addClass('slider').appendTo('div.custom-select');
-    $('div.custom-select').click(() => {
+    $(slider).addClass('slider').appendTo('div.custom-select');
+    $(CUSTOM_SELECT_OBJ).click(() => {
         if (animationEnd) {
             selectToggle();
         }
     });
 
     $(document).click(() => {
-        if (animationEnd && $('div.slider').css('display') === 'block') {
+        if (animationEnd && $(SLIDER_OBJ).css('display') === 'block') {
             selectToggle();
         }
     });
 
     function selectToggle() {
-        const slider = $('div.slider');
-        const customSelect = $('div.custom-select');
+        const slider = $(SLIDER_OBJ);
+        const customSelect = $(CUSTOM_SELECT_OBJ);
         if (slider.css('display') === 'none') {
             customSelect.toggleClass('custom-select--active');
         }
         animationEnd = false;
-        slider.slideToggle(500, () => {
+        slider.slideToggle(animationDelay, () => {
             animationEnd = true;
             if (slider.css('display') === 'none') {
                 customSelect.toggleClass('custom-select--active');
