e2b49feba464726c0ee72294d75e256c37c8ae50
Anatoliy Vasilev <toliankir@gmail.com>
2018-11-07 16:38:22 +0200

    Add scrollDown bool virable and scroll down listner

--- st2_perth/js/script.js
+++ st2_perth/js/script.js
@@ -2,6 +2,8 @@ const menuShowBtn = document.getElementById('menu-show');
 const menuHideBtn = document.getElementById('menu-hide');
 const toTopBtn = document.getElementById('button-up');
 const menu = document.getElementById('hidden-menu');
+let prevScrollValue = document.documentElement.scrollTop;
+let scrollDown = false;
 
 menuShowBtn.addEventListener('click', () => {
     menu.style.visibility = 'visible';
@@ -12,12 +14,21 @@ menuHideBtn.addEventListener('click', () => {
 });
 
 toTopBtn.addEventListener('click', () => {
+    scrollDown = false;
     scrollToTop();
+});
+
+window.addEventListener('scroll', () => {
+    const scrollValue = document.documentElement.scrollTop;
+    if (prevScrollValue < scrollValue) {
+        scrollDown = true;
+    }
+    prevScrollValue = scrollValue;
 });
 
 function scrollToTop() {
     const scrollPosition = document.documentElement.scrollTop;
-    if (scrollPosition > 0) {
+    if (scrollPosition > 0 && !scrollDown) {
         window.scrollTo(0, scrollPosition - (scrollPosition / 10));
         window.requestAnimationFrame(scrollToTop);
     }
