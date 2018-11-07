65bcd7620b0844511df22a4d086c72a152c95595
Anatoliy Vasilev <toliankir@gmail.com>
2018-11-07 13:55:28 +0200

    Add different style files for different resolution

--- st2_perth/js/script.js
+++ st2_perth/js/script.js
@@ -1,5 +1,6 @@
 const menuShowBtn = document.getElementById('menu-show');
 const menuHideBtn = document.getElementById('menu-hide');
+const toTopBtn = document.getElementById('button-up');
 const menu = document.getElementById('hidden-menu');
 
 menuShowBtn.addEventListener('click', () => {
@@ -9,3 +10,15 @@ menuShowBtn.addEventListener('click', () => {
 menuHideBtn.addEventListener('click', () => {
     menu.style.visibility = 'hidden';
 });
+
+toTopBtn.addEventListener('click', () => {
+    scrollToTop();
+});
+
+function scrollToTop() {
+    const scrollPosition = document.documentElement.scrollTop;
+    if (scrollPosition > 0) {
+        window.scrollTo(0, scrollPosition - (scrollPosition / 10));
+        window.requestAnimationFrame(scrollToTop);
+    }
+}
