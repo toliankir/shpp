289cdb68d7f23b54ea972b46b659f7a0b8400dc3
toliankir@gmail.com <toliankir@gmail.com>
2018-11-24 13:48:42 +0200

    First upload of st1.5

---
+++ webdev-st1.5-slider/slider-template.js
@@ -0,0 +1,12 @@
+const API_URL = 'https://picsum.photos/';
+const BIG_SIZE = '600/400';
+const SMALL_SIZE = '60';
+
+const IMAGES = [
+  '?image=1080', 
+  '?image=1079', 
+  '?image=1069', 
+  '?image=1063', 
+  '?image=1050',
+  '?image=1039'
+];
