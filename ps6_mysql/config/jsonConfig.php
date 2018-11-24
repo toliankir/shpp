c91be4aef82b1cde45ce1a04ac25a2487d3a2f2c
toliankir@gmail.com <toliankir@gmail.com>
2018-11-24 14:56:43 +0200

    Remove jsonService and jsonConfig

--- ps6_mysql/config/jsonConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-$rootDir = dirname(__DIR__) . DIRECTORY_SEPARATOR;
-$jsonDir = $rootDir . "json" . DIRECTORY_SEPARATOR;
-
-return array('chatData' => $jsonDir . 'chatdata.json',
-    'usersData' => $jsonDir . 'users.json');
