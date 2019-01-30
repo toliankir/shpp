8e5189901f084ac58e3d7d6e9857df7812530128
toliankir@gmail.com <toliankir@gmail.com>
2019-01-30 14:11:34 +0200

    Change regexp inpur form

--- ps7_regexp_logging/text_parser/script.js
+++ ps7_regexp_logging/text_parser/script.js
@@ -1,19 +1,24 @@
 const inputText = document.getElementById('input-text');
 const output = document.getElementById('output');
 const inputRegExp = document.getElementById('regexp');
+const inputRegExpFlag = document.getElementById('regexp_flags');
 
 document.addEventListener('input', () => {
+    inputRegExp.className = '';
+    inputRegExpFlag.className = '';
     try {
-        markText(inputRegExp.value);
-        inputRegExp.className = '';
-    } catch (e) {
-        markText();
+        markText(inputRegExp.value, inputRegExpFlag.value);
+    } catch (err) {
+        if (err.toString().indexOf('Invalid flags supplied') !== -1) {
+            inputRegExpFlag.className = 'error';
+            return;
+        }
         inputRegExp.className = 'error';
     }
 });
 
-function markText(regStr) {
-    const regExp = new RegExp(regStr, 'g');
+function markText(regStr, regFlag) {
+    const regExp = new RegExp(regStr, regFlag);
     output.innerHTML = inputText.value.replace(regExp, (el) => {
         return `<mark>${el}</mark>`;
     });
